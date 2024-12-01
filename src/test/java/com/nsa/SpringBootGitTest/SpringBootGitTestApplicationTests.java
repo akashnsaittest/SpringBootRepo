766fcda2da2103c6eee1d8642da1c7a6877475a7
akashnsaittest <akashkalamkar6959@gmail.com>
2024-12-01 20:07:46 +0530

    commiting spring boot project

---
+++ src/test/java/com/nsa/SpringBootGitTest/SpringBootGitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsa.SpringBootGitTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
